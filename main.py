2f90a41716b2be108c32d91e5963a33352fd4f88
Aditya <jetrax12@gmail.com>
2020-10-07 12:47:57 -0400

    Added instructions

--- main.py
+++ main.py
@@ -7,22 +7,38 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
+    def res_to_message(res):
+        return "Placeholder"
+
     def create_widgets(self):
+        message = "Welcome to Trope Detector 2020! Please enter a movie to see if it contains the following tropes:"
+        trope_list = ["Orientalism", "Gambit Pileup", "White Savior"]
+
+        self.intro = tk.Label(self, text=message)
+        self.intro.pack()
+        self.trope_list = tk.Label(self, text=', '.join(trope_list))
+        self.trope_list.pack()
+
+        self.movie_entry = tk.Entry(self)
+        self.movie_entry.pack()
+        self.moviename = tk.StringVar()
+        self.movie_entry["textvariable"] = self.moviename
+
         self.submit = tk.Button(self)
         self.submit["text"] = "Submit"
         self.submit["command"] = self.classify
         self.submit.pack()
-
-        self.movie_entry = tk.Entry(self)
-        self.movie_entry.pack(side="top")
-        self.moviename = tk.StringVar()
-        self.movie_entry["textvariable"] = self.moviename
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 
     def classify(self):
+        error_message = "Sorry, Link! I can't GIVE tropes. Come back when you're a little.... mmmmmmm... tropier!"
+        # try:
+        #   res = classifier.classify(self.moviename.get())
+        # except ValueError:
+        #   print("Oops! There was an issue processing the movie")
         print("hi there, everyone!" + self.moviename.get())
 
 root = tk.Tk()
